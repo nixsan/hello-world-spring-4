7718953300b7f628921c8e547a584a648dad88b6
nixsan <nix748@gmail.com>
2018-02-20 09:41:09 +0530

    Fixed typo for ;

--- demo/src/main/java/demo/HelloWorld.java
+++ demo/src/main/java/demo/HelloWorld.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class HelloWorld {
     public void sayHello(){
-        System.out.println("Hello World!!!!")
+        System.out.println("Hello World!!!!");
     }
 }
